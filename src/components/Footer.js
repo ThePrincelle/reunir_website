8978748fae71ab371702167b10566edf03369415
Maxime Princelle <maxime@princelle.org>
2022-01-27 10:39:47 +0100

    💬  adding LinkedIn link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,14 @@ import { getSingleton } from "../cms";
 export default function Footer(props) {
 	let [urlInstagram, setInstagramUrl] = useState("");
 	let [urlFacebook, setFacebookUrl] = useState("");
+	let [urlLinkedin, setLinkedinUrl] = useState("");
 
 	useEffect(() => {
 		getSingleton("contact")
 			.then((data) => {
 				setInstagramUrl(data.instagram);
 				setFacebookUrl(data.facebook);
+				setLinkedinUrl(data.linkedin);
 			})
 			.catch((err) => console.error(err));
 	}, []);
@@ -46,6 +48,12 @@ export default function Footer(props) {
 		</svg>
 	);
 
+	let linkedinIcon = (
+		<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16"	className="h-5 w-5"	aria-hidden="true">
+		<path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+		</svg>
+	)
+
 	return (
 		<div>
 			<div className="relative">
@@ -74,7 +82,7 @@ export default function Footer(props) {
 							</div>
 						))}
 					</nav>
-					<div className="mt-8 flex justify-center space-x-6">
+					<div className="mt-8 flex justify-center items-center space-x-6">
 						<a
 							key="facebook"
 							href={urlFacebook}
@@ -90,6 +98,14 @@ export default function Footer(props) {
 						>
 							<span className="sr-only">Instagram</span>
 							{instagramIcon}
+						</a>
+						<a
+							key="linkedin"
+							href={urlLinkedin}
+							className="text-gray-400 hover:text-gray-500 transition-colors"
+						>
+							<span className="sr-only">LinkedIn</span>
+							{linkedinIcon}
 						</a>
 					</div>
 					<p className="mt-8 text-center text-base text-gray-400">
