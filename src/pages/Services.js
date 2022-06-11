1f6eaa0c42fd729d869a7e97f25fef92bf8e4e54
Maxime Princelle <maxime@princelle.org>
2022-06-11 20:04:36 +0200

    ✨ add payment & teleconsulting infos (#14)
    * 💄 remove banner services pros
    
    * ✨ add profile pic in about page
    
    * ✨ add payment infos
    
    * ✨ add payment informations
    
    * 💄 restore original services bg color
    
    * 💄 remove subtitle on payment page
    
    * 💄 remove title margin on payment page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -6,17 +6,19 @@ import DOMPurify from "dompurify";
 
 import { Link, useHistory } from "react-router-dom";
 
-import { getCollection, getSingleton } from "../cms";
+import { getCollection } from "../cms";
 
 import Tabs from "../components/Tabs";
 import ServicesDetails from "../components/ServiceDetails";
+import PaymentModal from "../components/PaymentModal";
+import Teleconsulting from "../sections/services/Teleconsulting";
 
 export default function Services(props) {
 	let [services, setServices] = useState([]);
 	let [details, setDetails] = useState(false);
+	let [paymentsModal, setPaymentModal] = useState(false);
 	let [currentTab, setCurrentTab] = useState("Particuliers");
 	let [tabs, setTabs] = useState([]);
-	let [assetsServices, setAssetsServices] = useState(null);
 
 	useEffect(() => {
 		window.scrollTo(0, 0);
@@ -43,16 +45,17 @@ export default function Services(props) {
 				props.loader(false);
 			})
 			.catch((err) => console.error(err));
-		getSingleton("services")
-			.then((data) => {
-				setAssetsServices(data);
-			})
-			.catch((err) => console.error(err));
 	}, []);
 
 	let closeDetails = () => {
 		setTimeout(() => {
 			setDetails(false);
+		}, 250);
+	};
+
+	let closePaymentModal = () => {
+		setTimeout(() => {
+			setPaymentModal(false);
 		}, 250);
 	};
 
@@ -63,203 +66,217 @@ export default function Services(props) {
 	let history = useHistory();
 
 	return (
-		<div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-			<div className="absolute inset-0">
-				<div className="bg-white h-1/3 sm:h-2/3" />
-			</div>
-			<div className="relative max-w-7xl mx-auto">
-				<div className="text-center">
-					<h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
-						Services
-					</h2>
-					{/* <p className="hidden sm:inline-block mt-3 max-w-2xl mx-auto text-md sm:text-lg text-gray-500 sm:mt-4">
-						Vous trouverez ici la liste des services que je propose.
-					</p> */}
+		<div className="bg-gray-50 relative px-4 sm:px-6 lg:px-8">
+			<div className="pt-16 lg:pt-24">
+				<div className="absolute inset-0 z-0">
+					<div className="bg-white h-1/4 sm:h-2/4" />
 				</div>
-				{tabs.length > 1 && (
-					<Tabs
-						tabs={tabs}
-						currentTab={currentTab}
-						setCurrentTab={setTab}
-					/>
-				)}
-				{/*(currentTab == "Professionnels" && assetsServices && assetsServices.image) && (
-					<img
-						className="h-32 sm:h-64 rounded-lg mt-8 w-full object-cover bg-gray-100"
-						src={
-							"https://cms.re-unir.fr/api/cockpit/image?token=fbf36043e1aef774506461b27f1cd1&m=crop&w=1300&h=400&f[brighten]=10&o=true&fp=top&src=" +
-							assetsServices.image.path
-						}
-						alt=""
-					/>
-					)*/}
-				<div className="mt-8 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-					{services.map(
-						(service) =>
-							service.publish &&
-							service.type === currentTab && (
-								<div
-									key={service.titre}
-									className="flex flex-col rounded-lg shadow-md overflow-hidden transition-shadow hover:shadow-lg"
-									onClick={() => {
-										service.description_detaillee &&
-											setDetails(service);
-										// service.redirect && history.push(service.redirect)
-									}}
-								>
-									{service.image && "path" in service.image && (
-										<div className="flex-shrink-0 object-cover">
-											<img
-												className="h-48 w-full object-cover bg-gray-100"
-												src={
-													"https://cms.re-unir.fr/api/cockpit/image?token=fbf36043e1aef774506461b27f1cd1&m=thumbmail&w=400&h=180&f[brighten]=10&o=true&src=" +
-													service.image.path
-												}
-												alt=""
-											/>
-										</div>
-									)}
-									<div className="flex-1 bg-white p-6 flex flex-col justify-between">
-										<div className="flex-1 pb-5">
-											<p className="text-sm font-medium text-green-600">
-												{service.type}
-											</p>
-											<p className="text-xl font-semibold pt-2 text-gray-900">
-												{service.titre}
-											</p>
-											<div
-												className="mt-3 text-base prose prose-yellow text-gray-500 space-y-2"
-												dangerouslySetInnerHTML={{
-													__html: DOMPurify.sanitize(
-														service.description_courte
-													),
-												}}
-											></div>
-											{service.description_detaillee && (
-												<div className="pt-4 prose prose-yellow">
-													<a
-														onClick={() => {
-															setDetails(service);
-														}}
-													>
-														Plus d'informations...
-													</a>
+				<div className="relative max-w-7xl mx-auto">
+					<div className="text-center">
+						<h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+							Services
+						</h2>
+						{/* <p className="hidden sm:inline-block mt-3 max-w-2xl mx-auto text-md sm:text-lg text-gray-500 sm:mt-4">
+							Vous trouverez ici la liste des services que je propose.
+						</p> */}
+					</div>
+					{tabs.length > 1 && (
+						<Tabs
+							tabs={tabs}
+							currentTab={currentTab}
+							setCurrentTab={setTab}
+						/>
+					)}
+					{/*(currentTab == "Professionnels" && assetsServices && assetsServices.image) && (
+						<img
+							className="h-32 sm:h-64 rounded-lg mt-8 w-full object-cover bg-gray-100"
+							src={
+								"https://cms.re-unir.fr/api/cockpit/image?token=fbf36043e1aef774506461b27f1cd1&m=crop&w=1300&h=400&f[brighten]=10&o=true&fp=top&src=" +
+								assetsServices.image.path
+							}
+							alt=""
+						/>
+						)*/}
+					<div className="mt-8 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+						{services.map(
+							(service) =>
+								service.publish &&
+								service.type === currentTab && (
+									<div
+										key={service.titre}
+										className="flex flex-col rounded-lg shadow-md overflow-hidden transition-shadow hover:shadow-lg"
+										onClick={() => {
+											service.description_detaillee &&
+												setDetails(service);
+											// service.redirect && history.push(service.redirect)
+										}}
+									>
+										{service.image &&
+											"path" in service.image && (
+												<div className="flex-shrink-0 object-cover">
+													<img
+														className="h-48 w-full object-cover bg-gray-100"
+														src={
+															"https://cms.re-unir.fr/api/cockpit/image?token=fbf36043e1aef774506461b27f1cd1&m=thumbmail&w=400&h=180&f[brighten]=10&o=true&src=" +
+															service.image.path
+														}
+														alt=""
+													/>
 												</div>
 											)}
-
-											{service.redirect && (
-												<div className="pt-4 prose prose-yellow">
-													<a
-														onClick={() => {
-															history.push(
-																service.redirect
-															);
-														}}
-													>
-														Plus d'informations...
-													</a>
-												</div>
-											)}
-										</div>
-										{(service.duree_details ||
-											service.prix_details) && (
-											<div className="pt-3">
-												<div className="relative">
-													<div
-														className="absolute inset-0 flex items-center"
-														aria-hidden="true"
-													>
-														<div className="w-full border-t border-gray-300" />
-													</div>
-													<div className="relative flex justify-center">
-														<span className="px-2 bg-white text-sm text-gray-500">
-															Détails
-														</span>
-													</div>
-												</div>
-											</div>
-										)}
-										{service.duree_details && (
-											<div className="pt-1">
-												<div className="pt-1 italic text-gray-500 text-sm">
-													{service.duree_details}
-												</div>
-											</div>
-										)}
-										{service.prix_details && (
-											<div className="pt-1">
-												<div className="pt-1 italic text-gray-500 text-sm">
-													{service.prix_details}
-												</div>
-											</div>
-										)}
-										<div className="mt-4 flex items-center">
-											{service.prix && (
-												<div className="flex-shrink-0">
-													<span className="sr-only">
-														{service.prix}
-													</span>
-												</div>
-											)}
-											<div className="">
-												{service.prix && (
-													<p className="text-sm font-medium text-gray-900">
-														{service.prix}
-													</p>
-												)}
-												{service.duree && (
-													<div className="flex space-x-1 text-sm text-gray-500">
-														<span>
-															{service.duree}
-														</span>
-													</div>
-												)}
-											</div>
-											<div className="ml-auto align-center flex">
-												<Link to="/contact">
-													<button
-														type="button"
-														className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-green-700 bg-green-100 hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
-													>
-														Contact
-													</button>
-												</Link>
+										<div className="flex-1 bg-white p-6 flex flex-col justify-between">
+											<div className="flex-1 pb-5">
+												<p className="text-sm font-medium text-green-600">
+													{service.type}
+												</p>
+												<p className="text-xl font-semibold pt-2 text-gray-900">
+													{service.titre}
+												</p>
+												<div
+													className="mt-3 text-base prose prose-yellow text-gray-500 space-y-2"
+													dangerouslySetInnerHTML={{
+														__html: DOMPurify.sanitize(
+															service.description_courte
+														),
+													}}
+												></div>
 												{service.description_detaillee && (
-													<button
-														onClick={() =>
-															setDetails(service)
-														}
-														type="button"
-														className="inline-flex ml-2 items-center p-1.5 border border-transparent rounded-full shadow-sm text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
-													>
-														<PlusSmIconSolid
-															className="h-5 w-5"
-															aria-hidden="true"
-														/>
-													</button>
+													<div className="pt-4 text-yellow-600 hover:text-yellow-700">
+														<a
+															onClick={() => {
+																setDetails(
+																	service
+																);
+															}}
+															type="button"
+														>
+															Plus
+															d'informations...
+														</a>
+													</div>
 												)}
+
 												{service.redirect && (
-													<button
-														onClick={() =>
-															history.push(
-																service.redirect
-															)
-														}
-														type="button"
-														className="inline-flex ml-2 items-center p-1.5 border border-transparent rounded-full shadow-sm text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
-													>
-														<PlusSmIconSolid
-															className="h-5 w-5"
-															aria-hidden="true"
-														/>
-													</button>
+													<div className="pt-4 text-yellow-600 hover:text-yellow-700">
+														<a
+															onClick={() => {
+																history.push(
+																	service.redirect
+																);
+															}}
+															type="button"
+														>
+															Plus
+															d'informations...
+														</a>
+													</div>
 												)}
+											</div>
+											{(service.duree_details ||
+												service.prix_details) && (
+												<div className="pt-3">
+													<div className="relative">
+														<div
+															className="absolute inset-0 flex items-center"
+															aria-hidden="true"
+														>
+															<div className="w-full border-t border-gray-300" />
+														</div>
+														<div className="relative flex justify-center">
+															<span className="px-2 bg-white text-sm text-gray-500">
+																Détails
+															</span>
+														</div>
+													</div>
+												</div>
+											)}
+											{service.duree_details && (
+												<div className="pt-1">
+													<div className="pt-1 italic text-gray-500 text-sm">
+														{service.duree_details}
+													</div>
+												</div>
+											)}
+											{service.prix_details && (
+												<div className="pt-1">
+													<div className="pt-1 italic text-gray-500 text-sm">
+														{service.prix_details}
+													</div>
+												</div>
+											)}
+											<div className="mt-4 flex items-center">
+												{service.prix && (
+													<div className="flex-shrink-0">
+														<span className="sr-only">
+															{service.prix}
+														</span>
+													</div>
+												)}
+												<div className="">
+													{service.prix && (
+														<p className="text-sm font-medium text-gray-900">
+															{service.prix}
+														</p>
+													)}
+													{service.duree && (
+														<div className="flex space-x-1 text-sm text-gray-500">
+															<span>
+																{service.duree}
+															</span>
+														</div>
+													)}
+												</div>
+												<div className="ml-auto align-center flex">
+													<Link to="/contact">
+														<button
+															type="button"
+															className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-green-700 bg-green-100 hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+														>
+															Contact
+														</button>
+													</Link>
+													{service.description_detaillee && (
+														<button
+															onClick={() =>
+																setDetails(
+																	service
+																)
+															}
+															type="button"
+															className="inline-flex ml-2 items-center p-1.5 border border-transparent rounded-full shadow-sm text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+														>
+															<PlusSmIconSolid
+																className="h-5 w-5"
+																aria-hidden="true"
+															/>
+														</button>
+													)}
+													{service.redirect && (
+														<button
+															onClick={() =>
+																history.push(
+																	service.redirect
+																)
+															}
+															type="button"
+															className="inline-flex ml-2 items-center p-1.5 border border-transparent rounded-full shadow-sm text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+														>
+															<PlusSmIconSolid
+																className="h-5 w-5"
+																aria-hidden="true"
+															/>
+														</button>
+													)}
+												</div>
 											</div>
 										</div>
 									</div>
-								</div>
-							)
-					)}
+								)
+						)}
+					</div>
+				<div className="pb-10 pt-10 z-10">
+					<Teleconsulting setPaymentModal={setPaymentModal} />
+				</div>
 				</div>
 			</div>
 			{details && (
@@ -267,6 +284,9 @@ export default function Services(props) {
 					service={details}
 					closeDetails={closeDetails}
 				/>
+			)}
+			{paymentsModal && (
+				<PaymentModal closePaymentModal={closePaymentModal} />
 			)}
 		</div>
 	);
---
+++ src/sections/services/Teleconsulting.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import DOMPurify from "dompurify";
+
+import { getSingleton } from "../../cms";
+
+import { CreditCardIcon } from "@heroicons/react/solid";
+
+export default function Teleconsulting(props) {
+	const [title, setTitle] = useState("");
+	const [subtitle, setSubtitle] = useState("");
+	const [content, setContent] = useState("");
+
+	useEffect(() => {
+		getSingleton("teleconsulting")
+			.then((data) => {
+				setTitle(data.title);
+				setSubtitle(data.subtitle);
+				setContent(data.content);
+			})
+			.catch((err) => console.error(err));
+	}, []);
+
+	return (
+		<div className="">
+			<div className="max-w-7xl mx-auto py-12 px-0 sm:px-6 lg:py-16 lg:px-8 lg:flex-row lg:justify-between">
+				<h2 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+					<span className="block">{subtitle}</span>
+					<span className="block text-green-600">{title}</span>
+				</h2>
+				<p
+					className="prose prose-green prose-md mt-6"
+					dangerouslySetInnerHTML={{
+						__html: DOMPurify.sanitize(content),
+					}}
+				></p>
+				<div className="mt-8">
+					<div className="inline-flex rounded-md shadow w-2/3 sm:w-1/3 h-16">
+						<button
+							onClick={() => {
+								props.setPaymentModal(true);
+							}}
+							type="button"
+							className="inline-flex items-center min-w-full justify-center px-5 py-1 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700"
+						>
+							<CreditCardIcon className="w-12" />
+							<span className="ml-2">Paiement via PayPal</span>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
